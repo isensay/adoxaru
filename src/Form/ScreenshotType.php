d2151ba351ad9583b35a3d5d6a8ae22b265ae866
Petr Minkin <isensay@gmail.com>
2025-07-10 16:39:13 +0300

    change translate

--- src/Form/ScreenshotType.php
+++ src/Form/ScreenshotType.php
@@ -9,24 +9,34 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Url;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Contracts\Translation\TranslatorInterface;
+#use Symfony\Component\HttpFoundation\Request;
 
 class ScreenshotType extends AbstractType
 {
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('url', UrlType::class, [
                 'label' => false,
                 'constraints' => [
-                    new NotBlank(['message' => 'Пожалуйста, введите URL']),
+                    new NotBlank(['message' => $this->translator->trans('base.not_blank_message', [], 'screenshots')]),
                     new Url([
                         'protocols' => ['http', 'https'],
-                        'message' => 'Введите корректный URL'
+                        'message'   => $this->translator->trans('base.url_message', [], 'screenshots')
                     ]),
                 ],
                 'attr' => [
                     'placeholder' => 'https://example.com',
-                    'class' => 'form-control form-control-lg'
+                    'class' => 'form-control form-control-lg',
+                    'type' => 'url'
                 ]
             ]);
     }
