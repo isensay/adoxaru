238eb4e65cf3c99634ba93a9966c76724059d523
Petr Minkin <isensay@gmail.com>
2025-07-14 10:24:01 +0300

    add show system param

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,120 +7,53 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ExchangeRateRepository;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use App\Service\ExchangeRateService;
 
-#use App\Entity\ExchangeRate;
-#use Doctrine\ORM\EntityManagerInterface;
 
 final class HomeController extends AbstractController
 {
     private $translator;
+    private ExchangeRateService $exchangeRateService;
 
-    public function __construct(TranslatorInterface $translator) //, private EntityManagerInterface $em
-    {
+    public function __construct(
+        TranslatorInterface $translator,
+        ExchangeRateService $exchangeRateService
+    ) {
         $this->translator = $translator;
+        $this->exchangeRateService = $exchangeRateService;
     }
 
     #[Route('/', name: 'app_home')]
     public function index(ExchangeRateRepository $repository): Response
     {
-        $rates      = [];
-        $types      = [1 => 'usd', 2 => 'eur'];
-        $today      = date('d.m.Y');
-        $yesterday  = date('d.m.Y', time() - 86400);
-        $valuePrev = [];
+        // Получение информации о курсах валют
+        $rates = $this->exchangeRateService->getRates();
 
-        foreach ($repository->getLatestRates() as $rate) {
-            $nameType = $types[$rate->getType()];
-            if (!isset($rates[$nameType])) {
-                $date = date('d.m.Y', $rate->getDate());
-                $rates[$nameType] = [
-                    'value'      => round($rate->getValue(), 2),
-                    'difference' => null,
-                    'date'       => $date == $today ? $this->translator->trans('exchange_rates.date', [], 'home') : $date
-                ];
-            } else {
-                $valuePrev[$nameType] = round($rate->getValue(), 2);
-            }
-        }
+        // Получение информации о диске
+        $diskTotal = round(disk_total_space('/') / (1024 * 1024 * 1024), 2); // в ГБ
+        $diskFree  = round(disk_free_space('/')  / (1024 * 1024 * 1024), 2); // в ГБ
+        $diskUsed  = $diskTotal - $diskFree;
+        $diskUsagePercent = round(($diskUsed / $diskTotal) * 100);
 
-        foreach($rates as $nameType => $data) {
-            if (isset($valuePrev[$nameType])) {
-                $rates[$nameType]['difference'] = round($rates[$nameType]['value'] - $valuePrev[$nameType], 2);
-            }
-        }
-
-        foreach($types as $nameType)
-        {
-            if (!isset($rates[$nameType]))
-            {
-                $rates[$nameType] = [
-                    'value'      => null,
-                    'difference' => null,
-                    'date'       => null
-                ];
-            }
-        }
+        // Получение информации о памяти
+        $memoryInfo = file_get_contents('/proc/meminfo');
+        preg_match('/MemTotal:\s+(\d+)/', $memoryInfo, $matches);
+        $totalMem = round($matches[1] / (1024 * 1024)); // в ГБ
+        preg_match('/MemAvailable:\s+(\d+)/', $memoryInfo, $matches);
+        $freeMem = round($matches[1] / (1024 * 1024)); // в ГБ
+        $usedMem = $totalMem - $freeMem;
+        $memUsagePercent = round(($usedMem / $totalMem) * 100);
         
         return $this->render('pages/home.html.twig', [
+            'disk' => [
+                'total'        => $diskTotal,
+                'used_percent' => $diskUsagePercent
+            ],
+            'ram' => [
+                'total'        => $totalMem,
+                'used_percent' => $memUsagePercent
+            ],
             'rates' => $rates
         ]);
     }
-
-    /*
-    #[Route('/insert_rates', name: 'app_insert_rates')]
-    public function insert_rates(): Response
-    {
-        print '<html style="background:#282923; color:#ccc;">';
-
-        $rates = [];
-
-        foreach([1 => 'usd', 2 => 'eur'] as $typeId => $typeName)
-        {
-            $row = 1;
-
-            if (($handle = fopen("{$typeName}.csv", "r")) !== FALSE) {
-                while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-                    $num = count($data);
-
-                    $date  = preg_replace('/\x{FEFF}/u', '', $data[0]);
-                    $value = (float)$data[2];
-
-                    if (mb_strlen($date) == 10 || $value > 0)
-                    {
-                        $dt = explode('.', $date);
-                        $dateKey  = $dt[2].'-'.$dt[1].'-'.$dt[0];
-                        $unixtime = strtotime($dateKey.'T12:05:00');
-                        $rates[$dateKey][$typeId] = $value;
-                    }
-                    $row++;
-                }
-
-                fclose($handle);
-            }
-            
-        }
-
-        ksort($rates);
-
-        foreach($rates as $date => $dateArr)
-        {
-            $unixtime = strtotime($date.'T12:05:00');
-            foreach($dateArr as $type => $value)
-            {
-                $rate = new ExchangeRate();
-                $rate->setType((int)$type);
-                $rate->setValue((string)$value);
-                $rate->setDate((int)$unixtime);
-                $this->em->persist($rate);
-            }
-        }
-
-        $this->em->flush();
-
-        echo 'OK';
-
-        exit;
-    }
-    */
-    
 }
--- src/Service/ExchangeRateService.php
+++ src/Service/ExchangeRateService.php
@@ -5,17 +5,67 @@ namespace App\Service;
 use App\Entity\ExchangeRate;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ExchangeRateService
 {
     private const API_URL = 'https://www.cbr-xml-daily.ru/daily_json.js';
     private const MAX_RETRIES = 12; // 2 часа с интервалом 10 минут
     private const RETRY_INTERVAL = 600; // 10 минут
+    private $translator;
 
     public function __construct(
         private HttpClientInterface $client,
-        private EntityManagerInterface $em
-    ) {}
+        private EntityManagerInterface $em,
+        TranslatorInterface $translator
+    ) {
+        $this->translator = $translator;
+    }
+
+    public function getRates(): array
+    {
+        $rates      = [];
+        $types      = [1 => 'usd', 2 => 'eur'];
+        $today      = date('d.m.Y');
+        $yesterday  = date('d.m.Y', time() - 86400);
+        $valuePrev = [];
+
+        $repository = $this->em->getRepository(ExchangeRate::class);
+
+        foreach ($repository->getLatestRates() as $rate) {
+            $nameType = $types[$rate->getType()];
+            if (!isset($rates[$nameType])) {
+                $date = date('d.m.Y', $rate->getDate());
+                $rates[$nameType] = [
+                    'value'      => round($rate->getValue(), 2),
+                    'difference' => null,
+                    'date'       => $date == $today ? $this->translator->trans('exchange_rates.date', [], 'home') : $date
+                ];
+            } else {
+                $valuePrev[$nameType] = round($rate->getValue(), 2);
+            }
+        }
+
+        foreach($rates as $nameType => $data) {
+            if (isset($valuePrev[$nameType])) {
+                $rates[$nameType]['difference'] = round($rates[$nameType]['value'] - $valuePrev[$nameType], 2);
+            }
+        }
+
+        foreach($types as $nameType)
+        {
+            if (!isset($rates[$nameType]))
+            {
+                $rates[$nameType] = [
+                    'value'      => null,
+                    'difference' => null,
+                    'date'       => null
+                ];
+            }
+        }
+        
+        return $rates;
+    }
 
     public function updateRates(): bool
     {
